8dea7a89b7d5e83283fd5298db7ab6abbabd9479
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-08-08 20:31:15 +0530

    loader added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,15 +2,16 @@ import React, {useState} from 'react';
 import Navigationbar from './NavBar';
 import Main from './Main';
 import Users from './Users';
-import { usePromiseTracker } from "react-promise-tracker";
 import { trackPromise } from 'react-promise-tracker'
 import Loader from 'react-loader-spinner';
 
 function App() {
 
     const [users, setUsers] = useState([]);
+    const [isLoading, loaded] = useState(true);
 
     function getData() {
+
         trackPromise(
             fetch('https://reqres.in/api/users?page=1#')
                 .then(async function(userDetails) {
@@ -18,35 +19,38 @@ function App() {
                     setUsers(users.data);
                 })
         )
+
+        setTimeout(function() {
+            loaded(false);
+        },750);
     }
 
-    const LoadingIndicator = props => {
-           const { promiseInProgress } = usePromiseTracker();
-        
-           return (
-             promiseInProgress && 
-                 <div
-                   style={{
-                     width: "100%",
-                     height: "100",
-                     display: "flex",
-                     justifyContent: "center",
-                     alignItems: "center"
-                   }}
-                 >
-                   <Loader type="ThreeDots" color="#dc3545" height="100" width="100" />
-                 </div>
-          );  
-         }
+    function LoadingIndicator () {
+    
+        return (
+                <div
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginTop:"30vh"
+                }}
+                >
+                <Loader type="ThreeDots" color="#dc3545" height="150" width="150" />
+                </div>
+        );  
+    }
 
     return (
         <>
             <Navigationbar getUsers={getData} />
-            {users.length>0 ? 
-            <>
-                <LoadingIndicator/>
-                <Users users={users} />
-            </> : <Main />}
+            {
+                users.length>0 ? 
+                    (isLoading ? 
+                        <LoadingIndicator/> : <Users users={users} />
+                    ) : <Main />
+            }
         </>
     );
 }
